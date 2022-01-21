784d72362608a108eb58cd2d2788ea1ea2c40f8c
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2022-01-21 12:26:07 -0800

    help understanding the DOM model

---
+++ day-3/js/story.js
@@ -0,0 +1,9 @@
+const textarea = document.querySelector['textarea'];
+const button = document.querySelector['button'];
+
+console.log(textarea, button, document.body);
+
+const p = document.createElement('p');
+p.innerHTML = 'hello world';
+document.body.appendChild();
+textarea.value = '';
